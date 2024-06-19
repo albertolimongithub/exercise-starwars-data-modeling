b331f50ded3782745d925446e2679f887eba6252
Alberto Limón <119005684+albertolimongithub@users.noreply.github.com>
2024-06-19 22:13:00 +0000

    second commit

--- src/models.py
+++ src/models.py
@@ -5,8 +5,9 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+''' 
 Base = declarative_base()
-
+###
 class User(Base):
     __tablename__ = 'user'
     # Here we define columns for the table person
@@ -79,6 +80,71 @@ class Favorite(Base):
     name = Column(String(250), nullable=False)
     favorite_type = Column(String(250), nullable=False)
 
+
+    def to_dict(self):
+        return {}
+
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
+'''
+
+Base = declarative_base()
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), nullable=False)
+    population = Column(Integer)
+    gravity = Column(String(40))
+    climate = Column(String(50))
+    terrain = Column(String(50))
+    created = Column(String(50))
+    surface_water = Column(Integer)
+    diameter = Column(Integer)
+
+
+    def to_dict(self):
+        return {}
+
+class Character(Base):
+    __tablename__ = 'character'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(25), nullable=False)
+    created = Column(String(50))
+    homeworld = Column(String(50), ForeignKey(Planet.name))
+    eye_color = Column(String(10))
+    gender = Column(String(15))
+    hair_color = Column(String(20))
+    height = Column(Integer)
+    mass = Column(Integer)
+    skin_color = Column(String(20))
+
+    def to_dict(self):
+        return {}
+
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    username = Column(String(40), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+
+
+    def to_dict(self):
+        return {}
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    user_id = Column(Integer, ForeignKey(User.id))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    planet_name = Column(String(40), ForeignKey(Planet.name))
+    person_name = Column(String(25), ForeignKey(Character.name))
 
     def to_dict(self):
         return {}
