11cb888cf6ad6c2591dc3cb359b8f9c372ca946f
Alberto Limón <119005684+albertolimongithub@users.noreply.github.com>
2024-06-19 01:00:15 +0000

    first

--- src/models.py
+++ src/models.py
@@ -7,23 +7,78 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    nickname = Column(String(250), nullable=False)
+    password = Column(String(100), nullable=False)
+    age = Column(Integer)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planet(Base):
+    __tablename__ = 'planet'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    
+    user_id = Column(Integer, ForeignKey('person.id'))
+    user = relationship(User)
+
+    def to_dict(self):
+        return {}
+    
+class Character(Base):
+    __tablename__ = 'character'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    height = Column(Integer)
+    gender = Column(String(250))
+    hair_color = Column(String(250))
+    user_id = Column(Integer, ForeignKey('person.id'))
+    user = relationship(User)
+
+    def to_dict(self):
+        return {}
+    
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    vehicle_class = Column(String(250), nullable=False)
+    length = Column(String(250), nullable=False)
+
+
+    def to_dict(self):
+        return {}
+
+class Pilot(Base):
+    __tablename__ = 'pilot'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    vehicle_class = Column(String(250), nullable=False)
+
+
+    def to_dict(self):
+        return {}
+    
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    favorite_type = Column(String(250), nullable=False)
+
 
     def to_dict(self):
         return {}
